fc4bd17869c7c295b98f32b9e2849a08aa8553b1
rbrtknwls <r2knowle@uwaterloo.ca>
2021-03-13 09:38:58 -0500

    firebase set up

--- app.js
+++ app.js
@@ -6,13 +6,35 @@ var atob = require("atob");
 var Sent = require("sentiment");
 var admin = require("firebase-admin");
 const fetch = require("node-fetch");
+
 // CONSTANTS AND API KEYS
 const PORT = process.env.PORT || 3000;
-
 const speech = require('@google-cloud/speech');
-
-// Creates a client
 const client = new speech.SpeechClient();
+var serviceAccount = require("./shh.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://hackm-307513-default-rtdb.firebaseio.com"
+});
+
+// Instancate OBJECTS
+var app = express();
+var sent = new Sent();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+
+
+app.use(express.static(__dirname + "/public"));
+
+// PAGE BUILDING STUFF
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+
+server.listen(PORT);
+console.log("CHECKING PORT " + PORT);
 
 async function quickstart() {
   // The path to the remote LINEAR16 file
@@ -41,22 +63,15 @@ async function quickstart() {
 }
 quickstart();
 
+adduser(1);
+async function adduser(x) {
+  console.log(x);
 
-// Instancate OBJECTS
-var app = express();
-var sent = new Sent();
-var server = require("http").createServer(app);
-var io = require("socket.io")(server);
-
-
-
-app.use(express.static(__dirname + "/public"));
-
-// PAGE BUILDING STUFF
-app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname + "/index.html"));
-});
-
-
-server.listen(PORT);
-console.log("CHECKING PORT " + PORT);
+	admin.database().ref(x).set({
+		Name: 2,
+		Age: 17,
+		Emoji: "lol",
+		STT_Password: "My name is jeff hahaha funny",
+		Image_Comparison: "Some link to google"
+	});
+}
