ec89de9f9752d9853227116796e683e164ff2d78
rbrtknwls <r2knowle@uwaterloo.ca>
2021-03-14 05:11:14 -0400

    Backend mostly done!

--- Server/tools.js
+++ Server/tools.js
@@ -19,7 +19,7 @@ module.exports = {
   //   to the useres answers, will return a scale of how close
   //   the two texts are
 
-  Text_Closeness: async function (Corpus, Text) {
+  Text_Closeness: async function (Corpus, Text, callback) {
 
     var tagger = new pos.Tagger();
     var Testing= [];
@@ -122,48 +122,60 @@ module.exports = {
       }
 
     }
+    if (Corpus && Text){
+      var Target = (Corpus.split(" ")).length +0.0;
+      var Score = 0.0;
+      for (var x = 0; x < Answers.length; x++){
+        Not_Found = true;
+        for (var y = 0; y < Testing.length && Not_Found; y++){
+          if ((Answers[x].primary).localeCompare(Testing[y].primary) == 0) {
 
-    var Target = (Corpus.split(" ")).length +0.0;
-    var Score = 0.0;
-    for (var x = 0; x < Answers.length; x++){
-      Not_Found = true;
-      for (var y = 0; y < Testing.length && Not_Found; y++){
-        if ((Answers[x].primary).localeCompare(Testing[y].primary) == 0) {
+            balance = Math.max(Testing[y].mentions / Answers[x].mentions,1);
+            Score += Math.sqrt(balance) * Math.max(WEIGHT_OF_PRIMARY,
+              SFP*Target);
+              Not_Found = false;
 
-          balance = Math.max(Testing[y].mentions / Answers[x].mentions,1);
-          Score += Math.sqrt(balance) * Math.max(WEIGHT_OF_PRIMARY,
-            SFP*Target);
-            Not_Found = false;
-
-          } else{
-            for (var x1 = 0; x1 < Answers[x].length; x1++){
-              if ((Answers[x].secondary[x1]).localeCompare(Testing[y].primary) == 0) {
-                Score += WEIGHT_OF_SECONDARY;
-                Not_Found = false;
-                break;
+            } else{
+              for (var x1 = 0; x1 < Answers[x].length; x1++){
+                if ((Answers[x].secondary[x1]).localeCompare(Testing[y].primary) == 0) {
+                  Score += WEIGHT_OF_SECONDARY;
+                  Not_Found = false;
+                  break;
+                }
               }
             }
+
           }
-
         }
+
+
+        callback(Math.min((Score/Target)*100, 100));
+      }else{
+        callback(-1);
       }
-
-
-      return Math.min((Score/Target)*100, 100);
-
     },
 
-    Face_Compare: async function (x, callback) {
+    Local_Clear: async function (callback) {
 
       ClearCache(function(result1) {
         console.log("Cleared Local Ref");
         Update_Collection(function(result) {
           console.log("Populated Local Ref");
-          Compare_Face(x,function(result2) {
-            callback(result2);
-          });
+          //Compare_Face(x,function(result2) {
+          //callback(result2);
+          //});
+          callback();
         });
       });
+
+
+    },
+    Face_Compare: async function (x, callback) {
+
+      Compare_Face(x,function(result2) {
+        callback(result2);
+      });
+
 
 
     }
@@ -176,7 +188,6 @@ module.exports = {
           console.log(err, err.stack);
           return false;
         } else {
-          console.log("Made Collection: "+ collectionName);
           callback(true);
         }
       });
@@ -203,13 +214,15 @@ module.exports = {
           rek.indexFaces(locparams, function(err, img) {
             if (err){
               console.log(err, err.stack);
-              return false;
-            }
-            else{
-              callback(true);
+            }else{
+
             }
           });
         }
+        setTimeout(function(){
+          callback();
+        }, 1000);
+
       }
     });
   }
@@ -219,7 +232,7 @@ module.exports = {
       CollectionId: collectionName,
       Image: {
         S3Object: {
-          Bucket: "facecompare12",
+          Bucket: "localrawbucket",
           Name: face
         }
       },
@@ -232,7 +245,7 @@ module.exports = {
       }
 
       else{
-          callback(data.FaceMatches)
+        callback(data.FaceMatches)
       }
     });
   }
