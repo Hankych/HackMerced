d286c2903ec704b7d8c6af6dacf29c66b9e3d0d6
Hank Yang <hankm711sr@gmail.com>
2021-03-14 03:21:42 -0400

    Added Audio Record

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import Webcam from 'react-webcam';
 import axios from 'axios'
 import WebcamCapture from './components/WebcamCapture'
+import AudioRecord from './components/AudioRecord'
+
 
 export default class App extends Component{
 
     render(){
         return (
-            <WebcamCapture></WebcamCapture>
+            <div>
+                <div>
+                    <WebcamCapture></WebcamCapture>
+                </div>
+                <div>
+                    <AudioRecord></AudioRecord>
+                </div>
+            </div>
             )
     }
 }
